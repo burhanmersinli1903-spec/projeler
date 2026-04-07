2275bb1eb74927ed520d8d7fb93f26a21f3d6d89
Burhan MERSİNLİ <burhanmersinli1903@gmail.com>
2026-04-07 23:23:03 +0300

    Add files via upload

---
+++ PythonProject1/veri tipi dönüşümleri.py
@@ -0,0 +1,13 @@
+# sayi1=(input("Birinci sayı: "))
+# sayi2=(input("İkinci sayı: "))
+# toplam=sayi1+sayi2
+# print(toplam)
+#
+# ifade = "python"
+# ifade2 = ifade * 10
+# print(ifade2 )
+
+isim=(input("İsminizi giriniz = "))
+kitap=int(input("Bu yıl kaç kitap okudunuz = "))
+
+print(isim , ", bu yıl" ,kitap , " kitap okudu.")
