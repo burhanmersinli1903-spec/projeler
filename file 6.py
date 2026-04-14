816d6a0495e1d72bb895542f4ebb0280ee458156
Burhan MERSİNLİ <burhanmersinli1903@gmail.com>
2026-04-14 22:07:18 +0300

    Add files via upload

---
+++ file 6.py
@@ -0,0 +1,8 @@
+sifre= "Doğru olmayan herhangi birşey"
+
+while (sifre!="python"):#şifre python değilse programda kalacak.
+    sifre=input("Parolayı giriniz:")
+    if(sifre!="Python"):
+        print("Tekrar Deneyiniz=)")
+
+print("Giriş başarılı:)")
