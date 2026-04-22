3b51f1bb0a546964eb7fd309a3f0eaffda4ea36e
Burhan MERSİNLİ <burhanmersinli1903@gmail.com>
2026-04-22 21:50:44 +0300

    Add files via upload

---
+++ Githup dosya 3.py
@@ -0,0 +1,7 @@
+# for döngüsünü liste içinde kullanarak daha az kodla daha çok iş yapıyoruz. Bu yapı, veri bilimi ve otomasyon
+# projelerinde çoksık kullanılır.
+
+sayilar =[12, 45, 7, 23, 56, 89, 102]
+
+yeni_liste =["x**2 if x % 2 == 0" ]
+print(f"İşlenmiş liste: {yeni_liste}")
